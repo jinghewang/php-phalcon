cb789ef3f77618880b5efbffd3ef1bfccb9211dd
jinghewang <jinghewang@163.com>
2017-11-07 14:51:54 +0800

    modify sth

--- src/PhalconUtils.php
+++ src/PhalconUtils.php
@@ -13,6 +13,73 @@ class PhalconUtils
 {
 
     /**
+     * 获取 Phalcon 默认 di
+     * @author wjh 2017-11-07
+     *
+     * @return \Phalcon\DiInterface
+     */
+    public static function getDefaultDi() {
+        $di = \Phalcon\Di::getDefault();
+        return $di;
+    }
+
+
+    /**
+     * 注册volt 引擎
+     * @author wjh
+     * @date 20160420
+     * @param $c
+     */
+    public static function registerVoltEngines($c)
+    {
+        $di = \Phalcon\DI::getDefault();
+
+        //view
+        $c->view->registerEngines([
+            ".phtml" => function ($view, $di) {
+                $volt = new \Phalcon\Mvc\View\Engine\Volt($view, $di);
+                $volt->setOptions(
+                    [
+                        "compiledPath" => function ($templatePath) {
+                            $templatePath = str_replace('../module/','../cache/',$templatePath).'.compiled';
+                            $dir = dirname($templatePath);
+                            if (!is_dir($dir)) {
+                                mkdir($dir,0777,true);
+                            }
+                            //var_dump($templatePath);
+                            //var_dump($dir);
+                            //die;
+                            return $templatePath;
+                        },
+                        "compiledExtension" => ".compiled",
+                        "compileAlways" => true,
+                        "stat" => true,
+                    ]
+                );return $volt;
+            },
+            ".volt" => function ($view, $di) {
+                $volt = new \Phalcon\Mvc\View\Engine\Volt($view, $di);
+                $volt->setOptions(
+                    [
+                        "compiledPath" => function ($templatePath) {
+                            $templatePath = str_replace('../module/','../cache/',$templatePath).'.compiled';
+                            $dir = dirname($templatePath);
+                            if (!is_dir($dir)) {
+                                mkdir($dir,0777,true);
+                            }
+                            //var_dump($templatePath);
+                            //var_dump($dir);
+                            //die;
+                            return $templatePath;
+                        },
+                        "compiledExtension" => ".compiled",
+                    ]
+                );return $volt;
+            }
+        ]);
+    }
+
+    /**
      * hello
      * @author wjh 2017-11-06
      * @param string $message
